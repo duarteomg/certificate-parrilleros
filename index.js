d66d78f990ce1c6abf76142ea980d8bf545064f3
Alan Duarte <aduarte@omg.com.gt>
2020-09-08 07:30:09 -0600

    .

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 4000;
 const EmailCtrl = require('./mainCtrl');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 //email route
 app.post('/email', EmailCtrl.sendEmail);
