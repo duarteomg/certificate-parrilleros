886f8bcf4b66f0918e19f3496fa7d7512ee8d6a3
Alan Duarte <aduarte@omg.com.gt>
2020-09-08 07:39:03 -0600

    .

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const PORT = process.env.PORT || 4000;
 const EmailCtrl = require('./mainCtrl');
 
 app.use(bodyParser.json());
-app.use(cors());
+const whitelist = ['http://example1.com', 'http://example2.com']
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate));
 
 //email route
 app.post('/email', EmailCtrl.sendEmail);
