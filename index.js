7168d6b59bb9c2c692bfac203dd7b24764f9f198
Alan Duarte <aduarte@omg.com.gt>
2020-09-14 14:56:56 -0600

    .

--- index.js
+++ index.js
@@ -26,8 +26,3 @@ app.post('/email', EmailCtrl.sendEmail);
 app.listen(PORT, () => {
   console.log('server running on port ', PORT);
 })
-
-// emisor de particulas
-// capa de agua opcidad-10%
-// capa burbujas atrás
-// logo  
